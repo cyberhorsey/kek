f7b0bb2c1c31187d8b6c7c62be6782a3a1f5fafe
Jeffery Walsh <cyberhorsey@gmail.com>
2023-05-01 23:42:29 -0700

    audio

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -2,4 +2,8 @@
   <a class="btn btn-ghost normal-case text-xl">kek</a>
 
   <img src="./kek.png" class="mask mask-circle" style="height: 32px;" />
+
+  <audio loop autoplay>
+    <source src="./kek.mp3" type="audio/mpeg" />
+  </audio>
 </div>
